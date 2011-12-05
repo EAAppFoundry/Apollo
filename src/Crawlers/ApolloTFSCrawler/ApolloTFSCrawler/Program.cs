63abe8981b7fe0c398b735a41828071ce455bcaa
Allen Lyons <allen.lyons@gmail.com>
2011-12-05 14:22:53 -0500

    adds dummy doc to solr

---
+++ src/Crawlers/ApolloTFSCrawler/ApolloTFSCrawler/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Practices.ServiceLocation;
+using SolrNet;
+
+namespace ApolloTFSCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Startup.Init<RequirementsDocument>("http://localhost:8983/solr");
+
+            var solr = ServiceLocator.Current.GetInstance<ISolrOperations<RequirementsDocument>>();
+
+            var doc = new RequirementsDocument()
+                          {
+                              ID = "requirement1",
+                              Title = "This is the title.",
+                              Status = "Status1",
+                              Project = "Project 1",
+                              Department = "Department 1",
+                              SystemSource = "System 1",
+                              LastIndexed = DateTime.Now,
+                              Description =
+                                  "This is the description of the requirements. Here are some things to search for. FBD CID Orion"
+                          };
+
+            //do a commit just in case
+            solr.Commit();
+
+            solr.Add(doc);
+
+            solr.Commit();
+        }
+    }
+}
---
+++ src/Crawlers/ApolloTFSCrawler/ApolloTFSCrawler/RequirementsDocument.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using SolrNet.Attributes;
+
+namespace ApolloTFSCrawler
+{
+    public class RequirementsDocument
+    {
+        [SolrUniqueKey("id")] 
+        public string ID { get; set; }
+
+        [SolrField("title")]
+        public string Title { get; set; }
+
+        [SolrField("area")]
+        public string Area { get; set; }
+
+        [SolrField("status")]
+        public string Status { get; set; }
+
+        [SolrField("description")]
+        public string Description { get; set; }
+
+        [SolrField("relatedstories")]
+        public string RelatedStories { get; set; }
+
+        [SolrField("team")]
+        public string Team { get; set; }
+
+        [SolrField("releaseiteration")]
+        public string ReleaseIteration { get; set; }
+
+        [SolrField("storypoints")]
+        public string StoryPoints { get; set; }
+
+        [SolrField("acceptancecriteria")]
+        public string AcceptanceCriteria { get; set; }
+
+        [SolrField("iterationstart")]
+        public DateTime IterationStart { get; set; }
+
+        [SolrField("iterationend")]
+        public DateTime IterationEnd { get; set; }
+
+        [SolrField("discipline")]
+        public string Discipline { get; set; }
+
+        [SolrField("storytype")]
+        public string StoryType { get; set; }
+
+        [SolrField("storytags")]
+        public string StoryTags { get; set; }
+
+        [SolrField("storyuri")]
+        public string StoryURI { get; set; }
+
+        [SolrField("referenceid")]
+        public string ReferenceID { get; set; }
+
+        [SolrField("project")]
+        public string Project { get; set; }
+
+        [SolrField("department")]
+        public string Department { get; set; }
+
+        [SolrField("systemsource")]
+        public string SystemSource { get; set; }
+
+        [SolrField("lastindexed")]
+        public DateTime LastIndexed { get; set; }
+
+        [SolrField("lastupdated")]
+        public DateTime LastUpdated { get; set; }
+    }
+}
