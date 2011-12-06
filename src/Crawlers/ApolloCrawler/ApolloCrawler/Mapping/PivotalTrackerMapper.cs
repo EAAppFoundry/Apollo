67ee9aedffa6e45415285dafe6eaed4a11bca68e
Sri Srinivasan <sridevi.srinivasan@turner.com>
2011-12-06 12:00:32 -0500

    Adding the pivotal crawler mapper -rev 1

---
+++ src/Crawlers/ApolloCrawler/ApolloCrawler/Mapping/PivotalTrackerMapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApolloCrawler.Mapping
+{
+    public class PivotalTrackerMapper : IMapper
+    {
+        #region IMapper Members
+
+        public RequirementsDocument[] FindAllWorkItemsForProject()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
