97496ecd9f20f7b4e8858f63e07669f6ce69e7e7
Allen Lyons <allen.lyons@gmail.com>
2011-12-08 15:39:13 -0500

    Cleaned up url file for my sanity

--- src/ApolloWeb/Scripts/SolrUrl.js
+++ src/ApolloWeb/Scripts/SolrUrl.js
@@ -1,5 +1,10 @@
-﻿//var _solrHost = "http://localhost:8088/solr";
+﻿//sri
+//var _solrHost = "http://localhost:8088/solr";
+
+//allen
 var _solrHost = "http://localhost:8984/solr";
-//var _solrHost = "http://and:8989/solr-example"
-//var _solrHost = "http://eavdevcsearch01:8080/solr"
 var _codeSolrHost = "http://localhost:8983/solr";
+
+//dev server
+//var _solrHost = "http://eacdevrqsrch01:8080/solr"
+//var _codeSolrHost = "http://eavdevcsearch01:8080/solr";
