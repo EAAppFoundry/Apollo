ae4eae11dbef3a566ce797b07150b4f825ddb684
Allen Lyons <Allen.Lyons@gmail.com>
2012-02-03 16:28:45 -0500

    updated solr url for easier CI

--- src/ApolloWeb/Scripts/SolrUrl.js
+++ src/ApolloWeb/Scripts/SolrUrl.js
@@ -2,10 +2,10 @@
 //var _solrHost = "http://localhost:8088/solr";
 
 //allen
-var _solrHost = "http://localhost:8984/solr";
-var _codeSolrHost = "http://localhost:8983/solr";
+//var _solrHost = "http://localhost:8984/solr";
+//var _codeSolrHost = "http://localhost:8983/solr";
 
 //dev server
-//var _solrHost = "http://eacdevrqsrch01:8080/solr"
-//var _codeSolrHost = "http://eavdevcsearch01:8080/solr";
+var _solrHost = "http://eacdevrqsrch01:8080/solr"
+var _codeSolrHost = "http://eavdevcsearch01:8080/solr";
  
